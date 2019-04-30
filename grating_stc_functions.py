f6767e373efd1261672a5df02f9176831d96acbd
Vincent <vincent.a.leroux@gmail.com>
2019-04-30 14:21:18 +0200

    Initial commit

--- grating_stc_functions.py
+++ grating_stc_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.constants import c
+from scipy.interpolate import interp1d
 
 
 def build_omega_array(l0=8e-7, Nom=256, bandwidth=0.05, omega_span=20):
@@ -71,7 +72,13 @@ def grating_formula(alpha, om, gd=1 / 1.48e6):
         Inverse of the groove density in lines/m.
         Default groove density is 1480 lines/mm
     '''
-    return np.arcsin(np.sin(alpha) - np.sign(alpha) * 2 * np.pi * c / om / gd)
+    if isinstance(om, float):
+        om = np.array([om])
+    sin_beta = np.sin(alpha) - np.sign(alpha) * 2 * np.pi * c / om / gd
+    sin_beta[np.abs(sin_beta) > 1] = np.nan
+    if sin_beta.size == 1:
+        sin_beta = sin_beta[0]
+    return np.arcsin(sin_beta)
 
 
 def grating_distance(beta, om, phi2, gd=1 / 1.48e6):
@@ -98,10 +105,24 @@ def grating_distance(beta, om, phi2, gd=1 / 1.48e6):
     return gd**2 * phi2 * (np.cos(beta) * om)**3 / (8 * np.pi**2 * c)
 
 
-def divergence_from_phase(phase, x, k, hwhm):
+def divergence_from_phase(phase, x, k, hwhm, cut=0.5):
     '''
     Calculate beam divergence, radius of curvature
     and tilt from a 1D spatial phase.
+
+    Returns:
+    --------
+    divergence:
+        beam divergence in rad
+
+    radius_curvature:
+        radius of curvature in m
+
+    residual:
+        residuals of polynomial fit, to assess fit quality
+
+    slope:
+        Tilt of the phase, in rad
 
     Parameters:
     -----------
@@ -115,9 +136,301 @@ def divergence_from_phase(phase, x, k, hwhm):
         wave vector amplitude
 
     hwhm: float
-        Half-width at half maximum of the beam.
+        half-width at half maximum of the beam.
+
+    cut: float, optional
+        the phase above cut*hwhm is ignored
     '''
-    phase_fit, residual, _, _, _ = np.polyfit(x, phase, 4, full=True)
+    idx_cut = np.abs(x) <= cut * hwhm
+    x_cut = x[idx_cut]
+    phase_fit, residual, _, _, _ = np.polyfit(
+        x_cut, phase[idx_cut], 4, full=True)
     radius_curvature = -k / (2 * phase_fit[-3])
     divergence = np.arcsin(hwhm / radius_curvature)
-    return divergence, radius_curvature, residual[0], phase_fit[-2]
+    residual = residual[0]
+    slope = phase_fit[-2]
+    return divergence, radius_curvature, residual, slope
+
+
+def gauss2D(x, y, fwhmx, fwhmy, x0=0, y0=0, offset=0, order=1, int_FWHM=True):
+    '''
+    Define a (super-)Gaussian 2D beam.
+
+    Parameters:
+    -----------
+    x: float 2D np.array
+        Horizontal axis of the Gaussian
+
+    y: float 2D np.array
+        Vertical axis of the Gaussian
+
+    fwhmx: float
+        Horizontal Full Width at Half Maximum
+
+    fwhmy: float
+        Vertical Full Width at Half Maximum
+
+    x0: float, optional
+        Horizontal center position of the Gaussian
+
+    y0: float, optional
+        Vertical center position of the Gaussian
+
+    offset: float, optional
+        Amplitude offset of the Gaussian
+
+    order: int, optional
+        order of the super-Gaussian function.
+        Defined as: exp( - ( x**2 + y**2 )**order )
+
+    int_FWHM: boolean, optional
+        If True, the FWHM is the FWHM of the
+        square of the Gaussian (intensity).
+        If False, it is the FWHM of the Gaussian
+        directly (electric field).
+    '''
+    coeff = 1.0
+    if int_FWHM:
+        coeff = 0.5
+    gauss = np.exp(-np.log(2) * coeff *
+                   ((2 * (x - x0) / fwhmx) ** 2 +
+                    (2 * (y - y0) / fwhmy)**2)**order) + offset
+    return gauss
+
+
+def gauss1D(x, fwhm, x0=0, offset=0, order=1, int_FWHM=True):
+    '''
+    Define a (super-)Gaussian 1D beam.
+    Identical to gauss2D but in 1D.
+
+    Parameters:
+    -----------
+    x: float 1D np.array
+        Axis of the Gaussian
+
+    fwhm: float
+        Full Width at Half Maximum
+
+    x0: float, optional
+        Center position of the Gaussian
+
+    offset: float, optional
+        Amplitude offset of the Gaussian
+
+    order: int, optional
+        order of the super-Gaussian function.
+        Defined as: exp( - ( x**2 )**order )
+
+    int_FWHM: boolean, optional
+        If True, the FWHM is the FWHM of the
+        square of the Gaussian (intensity).
+        If False, it is the FWHM of the Gaussian
+        directly (electric field).
+    '''
+    coeff = 1.0
+    if int_FWHM:
+        coeff = 0.5
+    gauss = np.exp(-np.log(2) * coeff *
+                   ((2 * (x - x0) / fwhm)**2)**order) + offset
+    return gauss
+
+
+def get_stc_coeff(E_field_xyom, w0, om0, Dom, x, om, level=0.5):
+    '''
+    Calculate first order STC coefficients and temporal chirp
+
+    Returns:
+    --------
+    phi2:
+        temporal chirp in the center of the beam, in s2
+
+    zeta:
+        spatial chirp in x axis, in m.s/rad
+
+    beta:
+        angular chirp in x axis, in rad.s/rad
+
+    Parameters:
+    -----------
+    E_field_xyom: 3D numpy.array
+        data aranged in following order (y, x, omega)
+
+    w0: float
+        beam transverse waist in m,
+        used to limit calculation area
+
+    om0: float
+        central frequency in rad/s
+
+    Dom: float
+        half bandwith in rad/s,
+        used to limit calculation area
+
+    x: 1D numpy.array
+        x axis in m
+
+    om: 1D numpy.array
+        angular frequency axis in rad/s
+
+    level: float
+        signal amplitude below which data
+        is ignored for spatial chirp
+    '''
+
+    k0 = om0 / c
+    # indexes
+    idx_y = np.argmax(np.sum(np.sum(E_field_xyom, axis=-1), axis=-1))
+    idx_om = np.nonzero(np.abs(om - om0) < Dom)[0]
+    idx_x_arr = np.abs(x) < w0
+    idx_x = int(np.round(np.mean(idx_x_arr)))
+
+    # Spatial chirp
+    om_zeta, x_zeta = image_max_loc(np.abs(E_field_xyom).sum(
+        0), level=level, axis=0, x0=om, x1=x)
+    zeta = -np.polyfit(om_zeta, x_zeta, 1)[-2]
+
+    # Temporal chirp
+    phi2 = np.polyfit(om[idx_om], np.unwrap(
+        np.angle(E_field_xyom[idx_y, idx_x, idx_om])), 2)[-3] * 2
+
+    # Angular chirp
+    slope = np.zeros(idx_om.size)
+    for i, ii in enumerate(idx_om):
+        slope[i] = np.polyfit(x[idx_x_arr], np.unwrap(
+            np.angle(E_field_xyom[idx_y, idx_x_arr, ii])), 1)[-2]
+    beta = -np.polyfit(om[idx_om], slope, 1)[-2] / k0
+
+    return phi2, zeta, beta
+
+
+def get_pft(E_field_xyt, x, t, level=0.5):
+    '''
+    Calculate the PFT along the x-axis of a 3D E-field
+
+    Parameters:
+    -----------
+    E_field_xyt: 3D numpy.array
+        data aranged in following order (y, x, t)
+
+    x: 1D numpy.array
+        x axis in m
+
+    t: 1D numpy.array
+        time axis in s
+
+    level: float
+        signal amplitude below which data is ignored
+    '''
+    x_p, t_p = image_max_loc(np.abs(E_field_xyt).sum(0).T,
+                             level=level, axis=0, x0=x, x1=t)
+    p = np.polyfit(x_p, t_p, 1)[-2]
+    return p
+
+
+def image_max_loc(image, level=0.5, cut=None, axis=0, x0=None, x1=None):
+    '''
+    Gives the position of the maxima of an image along a given axis
+
+    Returns:
+    --------
+    x0_max: numpy.array
+        index of maxima on the first axis
+
+    x1_max: numpy.array
+        index of maxima on the second axis
+
+    Parameters:
+    -----------
+    image: 2D numpy.array
+        input image
+
+    level: float or None, optional
+        signal amplitude below which data is ignored
+
+    cut: float or None, optional
+        if level is None, reduces the data range by
+        cutting the axis at the cut value
+
+    axis: 0 or 1, optional
+        Axis along which the maxima are found
+
+    x0: numpy.array or None, optional
+        Position vector of the first axis
+
+    x1: numpy.array or None, optional
+        Position vector of the second axis
+    '''
+
+    if x0 is None:
+        x0 = np.arange(image.shape[0])
+    if x1 is None:
+        x1 = np.arange(image.shape[1])
+
+    if level is not None:
+        idx = np.sum(image, axis=axis) >= level * \
+            np.max(np.sum(image, axis=axis))
+    elif axis == 0:
+        idx = np.abs(x0) <= cut
+    elif axis == 1:
+        idx = np.abs(x1) <= cut
+
+    if axis == 0:
+        x0_max = x0[idx]
+        x1_max = (np.argmax(image[:, idx], axis=0) -
+                  x1.size / 2) * (x1[1] - x1[0])
+    elif axis == 1:
+        x1_max = x1[idx]
+        x0_max = (np.argmax(image[idx, :], axis=1) -
+                  x0.size / 2) * (x0[1] - x0[0])
+
+    return x0_max, x1_max
+
+
+def get_moments(image):
+    '''
+    Compute image centroid and statistical waist
+    from the intensity distribution.
+    Returns x0, y0, wx, wy
+
+    Parameters:
+    -----------
+    image: 2D numpy array
+    '''
+
+    # Build axes in pixels
+    ny, nx = image.shape
+    x, y = np.arange(nx), np.arange(ny)
+    X, Y = np.meshgrid(x, y)
+    # Zeroth moment
+    c0 = np.sum(image)
+    # First moments
+    cx = np.sum(X * image) / c0
+    cy = np.sum(Y * image) / c0
+    # Second centered moments
+    sx2 = np.sum((X - cx)**2 * image) / c0
+    sy2 = np.sum((Y - cy)**2 * image) / c0
+    return cx, cy, 2 * np.sqrt(sx2), 2 * np.sqrt(sy2)
+
+
+def get_fwhm(intensity, interpolation_factor=1, kind='cubic'):
+    '''
+    Get the Full Width at Half Maximum of the 1D intensity distribution
+
+    Parameters:
+    -----------
+    intensity: 1D numpy array
+        intensity distribution
+
+    interpolation_factor: int, optional
+        Interpolate the data for a more accurate calculation
+
+    kind : str or int, optional
+        Specifies the kind of interpolation as a string.
+        See documentation of scipy.interp1d
+    '''
+    position = np.arange(intensity.size)
+    pos_i = np.linspace(np.min(position), np.max(position),
+                        interpolation_factor * position.size)
+    inten_i = interp1d(position[:], intensity[:], kind=kind)
+    idx = (inten_i(pos_i) >= np.max(inten_i(pos_i)) * 0.5).nonzero()[0]
+    return pos_i[idx[-1] + 1] - pos_i[idx[0]]
--- grating_stc_main.py
+++ grating_stc_main.py
@@ -1,8 +1,12 @@
 import numpy as np
-import matplotlib.plyplot as plt
+import matplotlib.pyplot as plt
 from scipy.constants import c
-from grating_stc_paper_functions import build_omega_array, grating_formula, grating_distance, divergence_from_phase
+import h5py
+from scipy.interpolate import interp2d
+import grating_stc_functions as stc
+from time import time
 
+script_t0 = time()
 
 ################### Simulation input parameters ###################
 
@@ -35,18 +39,19 @@ tau_chirped = 200e-12  # Chirped pulse duration
 ################### Compute remaining parameters ###################
 
 # Frequency axis
-om, om0, Dom, Nom, k0 = build_omega_array(
+om, om0, Dom, Nom, k0 = stc.build_omega_array(
     l0=l0, Nom=Nom, bandwidth=rel_bandwidth, omega_span=freq_axis_span)
 tau_0 = 4 * np.log(2) / Dom  # Fourier limited pulse duration, in s
 phi2 = -tau_chirped / Dom  # GDD to compensate
 
 # Get diffracted angles axis and remove imaginary rays
-beta = grating_formula(alpha, om, gr_spacing)  # Difracted angles, in rad
+beta = stc.grating_formula(alpha, om, gr_spacing)  # Difracted angles, in rad
 om = om[np.isfinite(beta)]  # Remove imaginary rays
 k = om / c  # Wave vector amplitude, in rad/m
 Nom = om.size  # Size of frequency axis, without imaginary rays
 beta = beta[np.isfinite(beta)]  # Remove imaginary rays
-beta0 = grating_formula(alpha, om0, gr_spacing)  # Angle chief ray
+beta0 = stc.grating_formula(alpha, om0, gr_spacing)  # Angle chief ray
+idx_om0 = np.argmin(np.abs(om - om0))
 
 # Time axis (for Fourier transform)
 dt = 1 / np.ptp(om / 2 / np.pi)  # Spacing time axis, in s
@@ -68,10 +73,137 @@ yf = dyf * np.arange(Ny)  # Vertical axis, in m
 yf -= np.mean(yf)  # Centering
 
 # Grating separation, and horizontal offset on G2
-L_dist = grating_distance(beta0, om0, phi2)  # Normal to the gratings, in m
+L_dist = stc.grating_distance(beta0, om0, phi2)  # Normal to the gratings, in m
 dx = (np.tan(beta0) - np.tan(beta)) * \
     np.cos(alpha) * L_dist  # Hor. dispersion, in m
 
 
 ################### Grating deformation ###################
 # Can be replaced by measured or simulated data
+
+# Import gratings deformation from h5 file
+g1 = {}
+with h5py.File('data/G1_deformation.h5', 'r') as f:
+    for key in list(f.keys()):
+        g1[key] = f[key][...]
+g2 = {}
+with h5py.File('data/G2_deformation.h5', 'r') as f:
+    for key in list(f.keys()):
+        g2[key] = f[key][...]
+
+# Interpolate on the beam coordinates.
+g1_interp = interp2d(g1['x'] * np.cos(alpha), g1['y'],
+                     g1['data'], kind='cubic', fill_value=0)
+g2_interp = interp2d(g2['x'] * np.cos(alpha), g2['y'],
+                     g2['data'], kind='cubic', fill_value=0)
+phi_pass1 = g1_interp(x, y + H1 / 2)
+phi_pass2 = np.zeros([Ny, Nx, Nom])
+phi_pass3 = np.zeros([Ny, Nx, Nom])
+for idx in np.arange(Nom):
+    phi_pass2[:, :, idx] = g2_interp(x + dx[idx], y + H1 / 2)
+    phi_pass3[:, :, idx] = np.flipud(g2_interp(x + dx[idx], y - H1 / 2))
+phi_pass4 = np.flipud(g1_interp(x, y - H1 / 2))
+# 3rd and 4th passes are flipped by the roof mirror
+
+# Total wavefront accumulated from the compressor
+phi_comp = phi_pass1[:, :, None] + phi_pass2 + \
+    phi_pass3 + phi_pass4[:, :, None]
+
+################### Build Near field ###################
+
+E_nf = stc.gauss2D(x=X, y=Y, fwhmx=2 * w0, fwhmy=2 * w0, order=6)
+E_om = stc.gauss1D(x=om, fwhm=Dom, x0=om0, order=1)
+
+# Spatio-spectral near field
+E_nf_om = E_nf[:, :, None] * E_om[None, None, :] * \
+    np.exp(1j * k[None, None, :] * phi_comp)
+
+# Get GDD of the center of the beam
+phi2_nf, _, _ = stc.get_stc_coeff(E_nf_om, w0, om0, Dom / 2, x, om, level=0.5)
+
+# Get curvature of central wavelength
+_, rad_curv_x, _, _ = stc.divergence_from_phase(
+    np.unwrap(np.angle(E_nf_om[Ny // 2, :, idx_om0])), x, k0, w0)
+_, rad_curv_y, _, _ = stc.divergence_from_phase(
+    np.unwrap(np.angle(E_nf_om[:, Nx // 2, idx_om0])), y, k0, w0)
+rad_curv = (rad_curv_x + rad_curv_y) / 2
+
+# Remove GDD and curvature
+E_nf_om = E_nf_om * \
+    np.exp(1j * phi2_nf / 2 * (om - om0)**2)[None, None, :] * \
+    np.exp(1j * k[None, None, :] / (2 * rad_curv) * R[:, :, None]**2)
+
+# Spatio-spectral far field
+E_ff_om = np.fft.fftshift(np.fft.fft2(E_nf_om, axes=(0, 1)), axes=(0, 1))
+
+# Remove Group Delay to center the trace in time axis
+on_axis_phase = np.unwrap(
+    np.angle(E_nf_om[Ny // 2, Nx // 2, np.abs(om - om0) <= Dom / 2]))
+mean_gd = np.mean(np.diff(on_axis_phase)) / (om[1] - om[0])
+gd_comp = np.exp(-1j * mean_gd * (om - om0))
+E_ff_om = E_ff_om * gd_comp[None, None, :]
+
+# Spatio-temporal far field
+E_ff_t = np.fft.fftshift(np.fft.fft(E_ff_om, axis=-1), axes=-1)
+
+################### Beam properties ###################
+
+# Intensity profile
+I_ff_t = np.abs(E_ff_t)**2
+
+# Pulse duration
+tau = stc.get_fwhm(np.sum(I_ff_t, axis=(0, 1)), interpolation_factor=10) * dt
+# Waist in pixels
+_, _, wx, wy = stc.get_moments(np.sum(I_ff_t, axis=-1))
+# PFT
+pft_ff = stc.get_pft(E_ff_t, xf, t, level=0.5)
+# GDD, Spatial chirp, angular chirp
+phi2_ff, zeta_ff, beta_ff = stc.get_stc_coeff(
+    E_ff_om, wx, om0, Dom / 2, xf, om, level=0.5)
+# Waist in m
+wx *= dxf
+wy *= dyf
+
+################### Display ###################
+
+print('\nBeam parameters in focus:')
+print('-------------------------\n')
+print('Pulse duration: {:.2f} fs'.format(tau * 1e15))
+print('Waist x: {:.2f} µm'.format(wx * 1e6))
+print('Waist y: {:.2f} µm'.format(wy * 1e6))
+print('PFT: {:.2f} fs/µm'.format(pft_ff * 1e9))
+print('GDD: {:.2f} fs2'.format(phi2_ff * 1e30))
+print('Spatial chirp: {:.2f} mm/(rad/fs)'.format(zeta_ff * 1e18))
+print('Angular chirp: {:.2f} mrad/(rad/fs)'.format(beta_ff * 1e18))
+
+print('\nSimulation time: {:.2f} sec'.format(time() - script_t0))
+
+plt.subplot(1, 3, 1)
+plt.imshow(np.sum(I_ff_t, axis=0), cmap='YlGnBu_r', aspect='equal',
+           extent=[t[0] * 1e15, t[-1] * 1e15, xf[0] * 1e6, xf[-1] * 1e6])
+plt.xlim(-150, 150)
+plt.ylim(-150, 150)
+plt.xlabel('t (fs)')
+plt.ylabel('x (µm)')
+plt.title('T-X')
+
+plt.subplot(1, 3, 2)
+plt.imshow(np.sum(I_ff_t, axis=1), cmap='YlGnBu_r', aspect='equal',
+           extent=[t[0] * 1e15, t[-1] * 1e15, yf[0] * 1e6, yf[-1] * 1e6])
+plt.xlim(-150, 150)
+plt.ylim(-150, 150)
+plt.xlabel('t (fs)')
+plt.ylabel('y (µm)')
+plt.title('T-Y')
+
+plt.subplot(1, 3, 3)
+plt.imshow(np.sum(I_ff_t, axis=2), cmap='YlGnBu_r', aspect='equal',
+           extent=[xf[0] * 1e6, xf[-1] * 1e6, yf[0] * 1e6, yf[-1] * 1e6])
+plt.xlim(-150, 150)
+plt.ylim(-150, 150)
+plt.xlabel('x (µm)')
+plt.ylabel('y (µm)')
+plt.title('X-Y')
+
+plt.tight_layout()
+plt.show()
